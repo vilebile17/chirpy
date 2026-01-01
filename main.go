59be07b4d983ff6534182bbe9486109055a406f1
Ibrahim Somauroo <ibrahimsomauroo@proton.me>
2026-01-01 12:59:29 +0000

    Made a /healthz page

--- main.go
+++ main.go
@@ -8,13 +8,20 @@ import (
 func main() {
 	const port = "8080"
 
-	m := http.NewServeMux()
-	h := http.FileServer(http.Dir("."))
-	m.Handle("/", h)
+	mux := http.NewServeMux()
+	fileServerHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServerHandler))
 	server := http.Server{
 		Addr:    ":" + port,
-		Handler: m,
+		Handler: mux,
 	}
+
+	healthzHandler := func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		resp.WriteHeader(http.StatusOK)
+		resp.Write([]byte("OK"))
+	}
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	log.Fatal(server.ListenAndServe())
 }
